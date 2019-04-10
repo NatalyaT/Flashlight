2b52392802ee3f4bbe57a457839f3ae57d941415
Natalya <natalya.tulloch@brooklynsteamcenter.org>
2019-04-10 16:57:27 -0400

    actual project

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func light(_ sender: UISwitch) {
+        if sender.isOn == true {
+            screen.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        else{
+            screen.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+    @IBOutlet var screen: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
